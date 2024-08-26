eefd35398d36a9695e6de37815a0d06aaf182916
Honey-Joe <honeyjoe943@gmail.com>
2024-08-27 01:10:41 +0530

    lkjkl

--- src/components/Blogroute/TrendingArticles/TrendingArticles.jsx
+++ src/components/Blogroute/TrendingArticles/TrendingArticles.jsx
@@ -4,40 +4,6 @@ import TrendingCard from "../TrendingCard/TrendingCard";
 const TrendingArticles = () => {
   return (
     <>
-        <div className=" 2xl:container mx-auto">
-            <div className=" w-[90%] mx-auto">
-                <div className=" grid grid-cols-1 md:grid-cols-3">
-                    <div>
-                        
-                    </div>
-                    <div className=" col-span-2">
-                        <div>
-                            <h1 className=" text-[28px] font-[poppins] font-bold py-2">Trending Articles</h1>
-                            <hr className=" mb-2 p-[1.2px] bg-[#E3E3E3]"/>
-                        </div>
-                        <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 my-10">
-                            {
-                                TrendingCardApi.map((e,index)=>{
-                                    return(
-                                        <> 
-                                            <div key={index}>
-                                                <TrendingCard timg={e.timg}
-                                                    view={e.view}
-                                                    desc={e.desc}
-                                                    name={e.name}
-                                                    year={e.year}
-                                                    ctn={e.ctn}
-                                                />
-                                            </div>
-                                        </>
-                                    )
-                                })
-                            }
-                        </div>
-                    </div>
-                </div>
-            </div>
-        
       <div className="">
         <div className=" w-[100%]">
           <div>
