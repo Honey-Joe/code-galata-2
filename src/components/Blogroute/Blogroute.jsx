1448b890eae275c910d9cfdb674f7d833f1b8653
Honey-Joe <honeyjoe943@gmail.com>
2024-08-27 01:32:01 +0530

    asd

--- src/components/Blogroute/Blogroute.jsx
+++ src/components/Blogroute/Blogroute.jsx
@@ -1,10 +1,8 @@
 
 import Footer from "../Footer/Footer"
-import Header from "../Header/Header"
 import Navbar from "../Navbar/Navbar"
 import Blogsection from "./BlogSection/Blogsection"
 import Blogtab from "./Blogtab/Blogtab"
-import TrendingArticles from "./TrendingArticles/TrendingArticles"
 
 const Blogroute = () => {
   return (
@@ -12,7 +10,6 @@ const Blogroute = () => {
         
         <Navbar/>
 
-        <Header></Header>
         <Blogsection/>
         <Blogtab></Blogtab>
         <Footer/>
