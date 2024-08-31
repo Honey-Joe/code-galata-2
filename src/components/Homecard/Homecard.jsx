412f976c7325e04f6a2bc19215f324de42973eee
Honey-Joe <honeyjoe943@gmail.com>
2024-08-31 11:20:34 +0530

    elevated

--- src/components/Homecard/Homecard.jsx
+++ src/components/Homecard/Homecard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Homecard = ({ home }) => {
   return (
-    <div className='flex flex-col gap-2 items-center py-10 px-5 justify-center bg-white' id='certi'>
+    <div className='flex flex-col gap-2 items-center py-10 px-5 justify-center bg-white hover:scale-[1.01]' id='certi'>
       <div className=''>
         <img src={home.image} alt={home.name} className='' />
       </div>
