0d182d3007d588a65eb086df738c0d468d8e585a
Honey-Joe <honeyjoe943@gmail.com>
2024-09-02 19:32:02 +0530

    updated header bulb image

--- src/API/ImageApi.js
+++ src/API/ImageApi.js
@@ -7,7 +7,7 @@ export const line = "https://ik.imagekit.io/HoneyJoe/Eduvault/Eduvault/Group%201
 export const arrow= "https://ik.imagekit.io/HoneyJoe/Eduvault/Eduvault/line.png?updatedAt=1725281865442"
 export const homeri1 = "https://ik.imagekit.io/jjyo3gsee/HI-Study/image/Frame%20794.png?updatedAt=1724494989353"
 export const homeri2 = "https://ik.imagekit.io/jjyo3gsee/HI-Study/image/Frame%20796.png?updatedAt=1724494882841"
-export const sideblub = "https://ik.imagekit.io/jjyo3gsee/HI-Study/image/bulb-6.png?updatedAt=1724605662409"
+export const sideblub = "https://ik.imagekit.io/HoneyJoe/Eduvault/Eduvault/bulb.png?updatedAt=1725285857072"
 
 // blog section trending card img 
 export const card1 = "https://ik.imagekit.io/jjyo3gsee/HI-Study/image/Frame%20796.png?updatedAt=1724494882841"
