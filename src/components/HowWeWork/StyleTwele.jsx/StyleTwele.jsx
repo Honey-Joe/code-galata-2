07977b9b36d87a0ad21f1c8d50bdba6c07155270
Honey-Joe <honeyjoe943@gmail.com>
2024-09-06 19:08:39 +0530

    ds

--- src/components/HowWeWork/StyleTwele.jsx/StyleTwele.jsx
+++ src/components/HowWeWork/StyleTwele.jsx/StyleTwele.jsx
@@ -5,7 +5,7 @@ const StyleTwele = () => {
     <>
         <div className=" w-[90%] lg:w-[90%] mx-auto lg:py-[40px] py-[20px]">
             <div>
-                <h1 className=" text-start py-14 font-[poppins] font-bold text-[24px] md:text-[34px] text-[#192335]">How we <span className="text-[#1176F0] underline underline-offset-4">Work!</span> </h1>
+                <h1 className=" text-start py-5 font-[poppins] font-bold text-[24px] md:text-[34px] text-[#192335]">How we <span className="text-[#1176F0] underline underline-offset-4">Work!</span> </h1>
                 <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mb-12 gap-5 md:gap-6">
                 {
                     StyleTweleApi.map((e,index)=>{
